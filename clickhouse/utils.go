6cb7ba21df50664322c66915fb4a334db135ffba
matang <matang28@gmail.com>
2020-07-14 21:59:37 +0300

    Adding util method to convert clickhouse record to map[string]interface

--- clickhouse/utils.go
+++ clickhouse/utils.go
@@ -1,7 +1,7 @@
 package clickhouse
 
 func AsMap(record Record, mapping RecordMapping) map[string]interface{} {
-	var out map[string]interface{}
+	out := make(map[string]interface{})
 	for idx := range record {
 		out[mapping[idx]] = record[idx]
 	}
